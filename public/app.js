fe9796465a6e7a1aa29501e64829a47e0727c154
kharioki <antolisah@gmail.com>
2020-05-28 09:05:32 +0300

    dom and type casting

---
+++ public/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var anchor = document.querySelector('a');
+// console.log(anchor.href);
+// if (anchor) {
+//   console.log(anchor.href);
+// }
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+// console.log(form.children);
+// inputs
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#toFrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
